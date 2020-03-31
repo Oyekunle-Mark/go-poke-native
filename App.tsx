ab44206ba0654f7960e74dcdbfe166ec81fe9490
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-31 19:59:34 +0100

    Created the apollo client

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 export default function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh',
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   );
 }
 

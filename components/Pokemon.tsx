f3960578ac503f5e5737473baa6730f142138af5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-31 19:44:02 +0100

    Added more styles

--- components/Pokemon.tsx
+++ components/Pokemon.tsx
@@ -10,9 +10,9 @@ export default ({ pokemon }) => (
       <Text>{pokemon.maxHP}</Text>
       <Text>{pokemon.maxCP}</Text>
     </View>
-    <View className="pokemon__image">
-      <img src={pokemon.image} alt={pokemon.name} />
-    </View>
+
+    <img src={pokemon.image} alt={pokemon.name} />
+
     <View className="pokemon__attacks">
       {pokemon.attacks.special.slice(0, 3).map(attack => (
         <Text key={`${attack.name}-${attack.damage}`}>{attack.name}</Text>
@@ -22,13 +22,33 @@ export default ({ pokemon }) => (
 );
 
 const styles = StyleSheet.create({
-    pokemon: {
-        width: '90%',
-        backgroundColor: '#fff',
-        backgroundClip: 'border-box',
-        borderWeight: '1',
-        border$adius: '3',
-        overflow: 'hidden',
-        marginBottom: '20',
-    }
+  pokemon: {
+    width: 110,
+    backgroundColor: '#fff',
+    backgroundClip: 'border-box',
+    borderWeight: 1,
+    border$adius: 3,
+    overflow: 'hidden',
+    marginBottom: 20,
+  },
+  pokemonName: {
+    backgroundColor: '#ecd018',
+    textAlign: 'center',
+    padding: 10,
+  },
+  pokemonNameText: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: 'white',
+    letterSpacing: 4,
+  },
+  pokemonImage: {
+    padding: 20,
+    minHeight: 300,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    maxWidth: 100,
+    height: 'auto',
+  },
 });
